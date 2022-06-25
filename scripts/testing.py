75905a4bcf0e2698a68c4862d61b4a5edf46068a
sam23121 <smlalene@gmail.com>
2022-06-25 18:22:42 +0300

    adding extra files

--- scripts/testing.py
+++ scripts/testing.py
@@ -1,13 +1,16 @@
-import pandas as pd
+json = """
+[
+    "../data/USGS_LPC_ID_AdamsCounty_2019_B19_11TNK05644945.laz",
+    {
+        "type": "filters.sort",
+        "dimension": "X"
+    }
+]
+"""
 
-
-# df = pd.read_csv('data/metadata.csv', index_col='filename')
-# print(df.iloc[0])
-headers = [*pd.read_csv('data/metadata.csv', nrows=1)]
-df = pd.read_csv('data/metadata.csv', usecols=[c for c in headers if c != 'Unnamed: 0'], index_col='filename')
-print(df.iloc[0])
-# filename, xmin, ymin,xmax, ymax, points = df.loc[df['filename'] == 'AK_DeltaJunction_1_2021/']
-
-# file= df.iloc['AK_DeltaJunction_1_2021/']['filename']
-file = df.loc['AK_DeltaJunction_1_2021/', 'xmax'] 
-print(file)
+import pdal
+pipeline = pdal.Pipeline(json)
+count = pipeline.execute()
+arrays = pipeline.arrays
+metadata = pipeline.metadata
+log = pipeline.log
